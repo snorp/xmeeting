5271cbdcd1724beea0124c724276a52fd23ed66d
hfriederich <hfriederich>
2005-01-20 16:38:00 +0000

    Initial XCode-Project

---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  XMeeting
+//
+//  Created by Hannes on 20.1.2005.
+//  Copyright __MyCompanyName__ 2005. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
