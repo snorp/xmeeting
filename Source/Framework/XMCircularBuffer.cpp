313d689f97ae19133849009c93f29c536387ee0e
hfriederich <hfriederich>
2005-08-21 08:41:42 +0000

    Improving audio handling

---
+++ Source/Framework/XMCircularBuffer.cpp
@@ -0,0 +1,260 @@
+/*
+ * $Id: XMCircularBuffer.cpp,v 1.1 2005/08/21 08:41:42 hfriederich Exp $
+ *
+ * Copyright (c) 2005 XMeeting Project ("http://xmeeting.sf.net").
+ * All rights reserved.
+ * Copyright (c) 2005 Andreas Fenkart, Hannes Friederich. All rights reserved.
+ */
+
+#include "XMCircularBuffer.h"
+
+XMCircularBuffer::XMCircularBuffer(PINDEX len)
+: capacity(len + 1),  /* plus sentinel */
+head(0), 
+tail(0)
+{
+	cout << "XMCircularBuffer constructor " << (int)this << endl;
+	buffer = (char *)malloc(capacity*sizeof(char));
+	if(!buffer) 
+	{
+		running = false;
+	}
+	else
+	{
+		running = true;
+	}
+
+	/*
+	 * Mutex to block write thread when buffer is full
+	 */
+	int rval;
+	rval = pthread_mutex_init(&mutex, NULL);
+	if(rval != 0)
+	{
+		running = false;
+	}
+	  
+	rval = pthread_cond_init(&cond, NULL);
+	if(rval != 0) 
+	{
+		running = false;
+	}
+}
+
+XMCircularBuffer::~XMCircularBuffer()
+{	
+	cout << "XMCircularBuffer destructor " << (int)this << endl;
+	
+	// just to make sure
+	Stop();
+	
+	if(buffer != NULL)
+	{
+		std::free(buffer);
+		buffer = NULL;
+	}
+
+	pthread_mutex_destroy(&mutex);
+	pthread_cond_destroy(&cond);
+}
+ 
+BOOL XMCircularBuffer::Full()
+{
+	/* head + 1 == tail */
+	return head_next() == tail;
+}
+
+BOOL XMCircularBuffer::Empty()
+{
+	return head == tail;
+}
+
+PINDEX XMCircularBuffer::Size()
+{
+	/* sentinel is outside of occupied area */
+	return (head < tail ) ? head + capacity - tail : head - tail; 
+}
+
+PINDEX XMCircularBuffer::Free()
+{
+		return (capacity - Size() - 1 /*sentinel */);
+}
+
+PINDEX XMCircularBuffer::head_next()
+{
+	return ((head + 1) % capacity);
+}
+
+// increments the index by inc
+inline void XMCircularBuffer::increment_index(PINDEX &index, PINDEX inc)
+{
+	index += inc;
+	index %= capacity;
+}
+
+// increments the head
+inline void XMCircularBuffer::increment_head(PINDEX inc)
+{
+	increment_index(head, inc);
+}     
+    
+// increments the tail
+inline void XMCircularBuffer::increment_tail(PINDEX inc)
+{
+	increment_index(tail, inc);
+}
+
+int XMCircularBuffer::Fill(const char *inbuf, PINDEX len, Boolean lock, Boolean overwrite)
+{
+   int done = 0, todo = 0;
+
+   if(inbuf== NULL) // no valid buffer specified
+   {
+	   return 0;
+   }
+   
+   if(running == false)
+   {
+	   // we discard any samples but return len to act as if we stored all bytes
+	   return len;
+   }
+
+   while(done != len && !Full()) 
+   {
+	   if(head >= tail) 
+	   {
+		   // head unwrapped, fill from head till end of buffer
+		   if(tail == 0) /* buffer[capacity] == sentinel */
+		   {
+			   todo = MIN(capacity -1 /*sentinel*/ - head, len - done);
+		   }
+		   else
+		   {
+			   todo = MIN(capacity - head, len - done);
+		   }
+	   } 
+	   else 
+	   {
+         // fill from head till tail 
+		   todo = MIN(tail -1 /*sentinel*/ - head, len - done);
+	   }
+	   memcpy(buffer + head, inbuf + done, todo);
+	   done += todo;
+	   increment_head(todo);
+   }
+
+   // What to do if buffer is full and more bytes
+   // need to be copied ?  
+   if(Full() && done != len && (overwrite || lock)) 
+   {
+	   if(lock) 
+	   {
+		   pthread_mutex_lock(&mutex);
+		   
+		   // wait until buffer is available. Do the necessary check again before
+		   // going to sleep
+		   if(Full())
+		   {
+			   pthread_cond_wait(&cond, &mutex);
+		   }
+		   pthread_mutex_unlock(&mutex);
+	   }
+	   else if(overwrite)
+	   {
+		   pthread_mutex_lock(&mutex);
+		   if(Full())
+		   {
+			   tail += len - done; // also shifts sentinel
+			   tail %= capacity; // wrap around
+		   }
+		   pthread_mutex_unlock(&mutex);
+	   }
+	   
+	   // try again
+	   done += Fill(inbuf + done, len - done, lock, overwrite);
+   }
+
+   // wake up read thread if necessary
+   if(!Empty())
+   {
+      pthread_cond_signal(&cond);
+   }
+   
+   return done;
+}
+
+
+PINDEX XMCircularBuffer::Drain(char *outbuf, PINDEX len, Boolean lock) 
+{
+   PINDEX done = 0, todo = 0;
+
+	if(outbuf == NULL || running == false)
+	{
+		return 0;
+	}
+
+	/* protect agains buffer corruption when write thread
+	 * is overwriting */
+	pthread_mutex_lock(&mutex);
+
+	while(done != len && !Empty()) 
+	{
+		if(head >= tail) 
+		{
+			// head unwrapped 
+			todo = MIN(head - tail, len - done);
+		}
+		else 
+		{
+			// head wrapped 
+			todo = MIN(capacity - tail, len - done);
+		}        
+		memcpy(outbuf + done, buffer + tail, todo);
+		done += todo;
+		increment_tail(todo);
+	}
+
+	// what to do if not as many bytes are available as
+	// requested ? If lock is true, we block until more data
+	// is available and then try again
+	if(done != len && (lock)) /* && Empty() */
+	{
+		if (lock)
+		{
+			if(!Full())
+			{
+				pthread_cond_wait(&cond, &mutex);
+			}
+			pthread_mutex_unlock(&mutex); // race with write thread...
+			done += Drain(outbuf + done, len - done, lock);
+			return done;
+		}
+	}
+
+	pthread_mutex_unlock(&mutex);
+
+	if(!Full())
+	{
+      pthread_cond_signal(&cond);
+	}
+	
+	return done;
+}
+
+void XMCircularBuffer::Stop()
+{
+	pthread_mutex_lock(&mutex);
+	running = false;
+	pthread_mutex_unlock(&mutex);
+	
+	pthread_cond_broadcast(&cond);
+}
+
+void XMCircularBuffer::Restart()
+{
+	pthread_mutex_lock(&mutex);
+	head = 0;
+	tail = 0;
+	running = true;
+	pthread_mutex_unlock(&mutex);
+}
---
+++ Source/Framework/XMCircularBuffer.h
@@ -0,0 +1,102 @@
+/*
+ * $Id: XMCircularBuffer.h,v 1.1 2005/08/21 08:41:42 hfriederich Exp $
+ *
+ * Copyright (c) 2005 XMeeting Project ("http://xmeeting.sf.net").
+ * All rights reserved.
+ * Copyright (c) 2005 Andreas Fenkart, Hannes Friederich. All rights reserved.
+ */
+
+#ifndef __XM_CIRCULAR_BUFFER_H__
+#define __XM_CIRCULAR_BUFFER_H__
+
+/**
+ * Simple circular buffer for one producer and consumer with a head and a 
+ * tail that chase each other. The capacity is 1 byte bigger than necessary 
+ * due to a sentinel, to tell apart full from empty by the following equations:
+ *
+ * full  := head_next == tail
+ * empty := head      == tail
+ *
+ * Inspired by CircularBuffer from beaudio.
+ * We need a lock when updating the tail index, due to the overwrite mechanism
+ * in case of buffer overflow. The head index needs no locking because 
+ * overwrite does not make sense in case of buffer underrun. 
+ *
+ * Keep in mind that this buffer does no know about frames or packets, it's up
+ * to you to make sure you get the right number of bytes. In doubt use size() 
+ * to compute how many frames/packets it is save to drain/fill.
+ *
+ * This buffer does also offer the chance to *stop* the buffer from being used
+ * through calling Stop(). After that, any bytes sent to Fill() will just be
+ * ignored and Drain returns 0 bytes constantly. This method also unblocks any
+ * thread locked when calling Fill() or Drain(). So, Stop() allows a third
+ * to interfere with the buffer behaviour and to remove any deadlocks.
+ * The call to Restart() will undo the operation above and *start* the buffer
+ * again. However, the buffer is considered empty after Restart()
+ **/
+
+class XMCircularBuffer
+{
+public:
+
+	explicit XMCircularBuffer(PINDEX len);
+	~XMCircularBuffer();
+ 
+	/**
+	 * Returns whether the buffer is full/empty or not 
+	 **/
+	BOOL Full();
+	BOOL Empty();
+
+	/**
+	 * Returns the Size (in bytes) for this buffer
+	 **/
+	PINDEX Size();
+	
+	/**
+	 * Returns the number of free bytes for this buffer
+	 **/
+	PINDEX Free();
+
+	/** 
+	 * Fill inserts data into the circular buffer. 
+     * Remind that lock and overwrite are mutually exclusive. If you set lock,
+     * overwrite will be ignored. Returns the amount of bytes actually
+	 * written to the buffer. If lock is true, this function blocks
+	 * until all bytes have been written (or the buffer was *stopped*)
+     **/
+	PINDEX Fill(const char* inbuf, PINDEX len, Boolean lock = true, 
+				Boolean overwrite = false);
+
+
+	/** 
+	 * See also Fill.
+	 * Returns the amount of bytes read from the buffer. If lock is true,
+	 * blocks until all bytes have been obtained. Note that even if lock
+	 * is set, this method may return a number smaller than len if the
+	 * buffer was *stopped* in the meantime
+	 **/
+	PINDEX Drain(char* outbuf, PINDEX len, Boolean lock = true);
+	
+	/**
+	 * *Starts* / *Stops* the buffer as desired
+	 **/
+	void Stop();
+	void Restart();
+
+ private:
+	inline PINDEX head_next();
+	inline void increment_index(PINDEX &index, PINDEX inc);
+	inline void increment_head(PINDEX inc);    
+	inline void increment_tail(PINDEX inc);
+	
+   char* buffer;
+   const PINDEX capacity;
+   PINDEX head, tail;
+   Boolean running;
+
+   pthread_mutex_t mutex;
+   pthread_cond_t cond;
+};
+
+#endif // __XM_CIRCULAR_BUFFER_H__
