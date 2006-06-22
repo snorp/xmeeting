9fec8dfabdf5a73d3ca9b41161b7a5d15594e56b
hfriederich <hfriederich>
2006-06-22 10:13:01 +0000

    Fixing display of long text that doesn't fit within a line

--- Source/GUI/XMCallInfoView.m
+++ Source/GUI/XMCallInfoView.m
@@ -1,5 +1,5 @@
 /*
- * $Id: XMCallInfoView.m,v 1.8 2006/06/21 22:54:10 hfriederich Exp $
+ * $Id: XMCallInfoView.m,v 1.9 2006/06/22 10:13:01 hfriederich Exp $
  *
  * Copyright (c) 2005-2006 XMeeting Project ("http://xmeeting.sf.net").
  * All rights reserved.
@@ -61,6 +61,7 @@
 	[textDrawCell setDrawsBackground:NO];
 	[textDrawCell setBezeled:NO];
 	[textDrawCell setBordered:NO];
+	[textDrawCell setLineBreakMode:NSLineBreakByTruncatingTail];
 	
 	[self _calculateDisclosureRect:frameRect];
 	
