0c7d98751e7107abcb0115bedb352810281248cd
hfriederich <hfriederich>
2008-09-22 22:26:09 +0000

    - MINOR: Workaround for PTLib linker errors

--- Source/Framework/XMProcess.cpp
+++ Source/Framework/XMProcess.cpp
@@ -1,5 +1,5 @@
 /*
- * $Id: XMProcess.cpp,v 1.9 2007/08/07 17:09:58 hfriederich Exp $
+ * $Id: XMProcess.cpp,v 1.10 2008/09/22 22:26:09 hfriederich Exp $
  *
  * Copyright (c) 2005-2007 XMeeting Project ("http://xmeeting.sf.net").
  * All rights reserved.
@@ -7,6 +7,12 @@
  */
 
 #include "XMProcess.h"
+
+// keep PTLib-linking happy
+namespace PWLibStupidOSXHacks {
+  int loadFakeVideoStuff;
+  int loadCoreAudioStuff;
+}
 
 XMProcess::XMProcess() 
 : PProcess("XMeeting Project", "XMeeting", 0, 4, AlphaCode, 1) 
